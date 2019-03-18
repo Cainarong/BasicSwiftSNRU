af41a3874fb8e3feb93e69bd22694ea61464ce06
masterToey <masterToey@abc.com>
2019-03-18 14:28:14 +0700

    Start DataType

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+//Sting Type  ตัวแปรที่เป็นอักษร
+
+var myName = "Doramon" //นี้คือการประกาศ ที่อาศัยชนิดของ Value บอก Datatype
+var mySurname:String = "AAAAAA" //นีเคือการประกาศแบบกำหนดDatatypeตั้งแต่เกิด
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//Explicit Variasble
+//การประกาศตัวแปร ครับ
+
+var  myName = "AzSuker"
+myName = "Doramon"
+
+    //Explicit Constant
+let myfriend = "Nopita"
+//myfriend = "sunako"Error Because myFrirnd is let
+
+
+
+
